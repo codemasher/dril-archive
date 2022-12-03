7cff65c877e078743f3b105fb5340dfe715bb800
smiley <codemasher@users.noreply.github.com>
2022-12-03 19:44:19 +0100

    :octocat:

--- src/build.php
+++ src/build.php
@@ -43,17 +43,18 @@ $options = new DrilArchiveOptions([
 	'outdir'                  => __DIR__.'/../output',
 	'fromCachedApiResponses'  => true,
 	'apiToken'                => getToken(),
-	'fetchFromAdaptiveSearch' => true,
-	'adaptiveRequestToken'    => 'AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA',
-	'adaptiveGuestToken'      => '1599107320918822914',
-	'query'                   => 'from:dril include:nativeretweets',
-#	'fetchFromAPISearch'      => true,
-#	'query'                   => sprintf('from:dril include:nativeretweets since:%s until:%s', date('Y-m-d', ($now - 86400 * 7)), date('Y-m-d', $now)),
+	'fetchFromAdaptiveSearch' => false,
+#	'adaptiveRequestToken'    => 'AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA',
+#	'adaptiveGuestToken'      => '1599107320918822914',
+#	'query'                   => 'from:dril include:nativeretweets',
+	'fetchFromAPISearch'      => true,
+	'query'                   => sprintf('from:dril include:nativeretweets since:%s until:%s', date('Y-m-d', ($now - 86400 * 7)), date('Y-m-d', $now)),
 #	'drilCSV'                 => realpath(__DIR__.'/../.build/dril.csv'), // https://docs.google.com/spreadsheets/d/1juZ8Dzx-hVCDx_JLVOKI1eHzBlURHd7u6dqkb3F8q4w
 #	'' => '',
 ]);
 
-$timelineJSON = null;
+// we need this one here just for the first run in order to convert to the new format
+$timelineJSON = __DIR__.'/../.build/dril.json';
 
 // on GitHub actions: clone repo, checkout gh-pages, use previous build
 if(isset($_SERVER['GITHUB_ACTIONS'])){
