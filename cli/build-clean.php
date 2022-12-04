e78f2728a747a6b7a22b9fc3d620f3b57a7af8ab
smiley <codemasher@users.noreply.github.com>
2022-12-04 17:06:22 +0100

    :octocat:

---
+++ cli/build-clean.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Creates a new archive from scratch
+ *
+ * @created      04.12.2022
+ * @author       smiley <smiley@chillerlan.net>
+ * @copyright    2022 smiley
+ * @license      WTFPL
+ */
+
+use codemasher\DrilArchive\DrilArchive;
+use codemasher\DrilArchive\DrilArchiveOptions;
+
+ini_set('date.timezone', 'UTC');
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+/*
+ * The bearer token from the developer account won't work here. How to get the request token:
+ *
+ *   - open https://twitter.com/search in a webbrowser (chrome or firefox recommended), ideally in an incognito tab
+ *   - open the developer console (press F12)
+ *   - type anything in the twitter search box, hit enter
+ *   - go to the "network" tab in the dev console and filter the requests for "adaptive.json"
+ *   - click that line, a new tab for that request appears
+ *   - there, in the "headers" tab, scroll to "request headers" and look for "Authorization: Bearer ..."
+ *   - right click that line, select "copy value" and paste it below, should look like: 'Bearer AAAANRILgAAAAAAnNwI...'
+ *
+ * It seems that the bearer token is valid for several days at least.
+ */
+$requestToken = 'Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA';
+
+/*
+ * The guest token is in the same place as the bearer token, just scroll a bit further
+ * and look for the "x-guest-token" header and copy that value.
+ *
+ * It appears that the guest token is valid for about 2 hours.
+ */
+$guestToken = '1599421023828459523';
+
+/*
+ * The search query
+ *
+ * @see https://developer.twitter.com/en/docs/twitter-api/tweets/search/integrate/build-a-query
+ * @see https://help.twitter.com/en/using-twitter/advanced-tweetdeck-features
+ *
+ * try:
+ *   - "@username" timeline including replies
+ *   - "@username include:nativeretweets filter:nativeretweets" for RTs only (returns RTs of the past week only)
+ *   - "to:username" for @mentions and replies
+ */
+$query = 'from:dril include:nativeretweets';
+
+$options = new DrilArchiveOptions;
+// HTTPOptions
+$options->ca_info                 = realpath(__DIR__.'/../config/cacert.pem'); // https://curl.haxx.se/ca/cacert.pem
+$options->user_agent              = 'drilArchive/1.0 +https://github.com/codemasher/dril-archive';
+// DrilArchiveOptions
+$options->builddir                = __DIR__.'/../.build';
+$options->outdir                  = __DIR__.'/../output';
+$options->fromCachedApiResponses  = true;
+$options->fetchFromAdaptiveSearch = true;
+$options->adaptiveRequestToken    = $requestToken;
+$options->adaptiveGuestToken      = $guestToken;
+$options->query                   = $query;
+// https://docs.google.com/spreadsheets/d/1juZ8Dzx-hVCDx_JLVOKI1eHzBlURHd7u6dqkb3F8q4w
+$options->drilCSV = realpath(__DIR__.'/../.build/dril.csv');
+
+
+(new DrilArchive($options))->compileDrilTimeline();
+
+exit;
---
+++ cli/incremental-update.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Runs an incremental update for an existing archive
+ *
+ * @created      28.11.2022
+ * @author       smiley <smiley@chillerlan.net>
+ * @copyright    2022 smiley
+ * @license      WTFPL
+ */
+
+use chillerlan\DotEnv\DotEnv;
+use codemasher\DrilArchive\DrilArchive;
+use codemasher\DrilArchive\DrilArchiveOptions;
+
+ini_set('date.timezone', 'UTC');
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+
+/*
+ * The search query
+ *
+ * @see https://developer.twitter.com/en/docs/twitter-api/tweets/search/integrate/build-a-query
+ * @see https://help.twitter.com/en/using-twitter/advanced-tweetdeck-features
+ */
+
+$now   = time();
+$since = $now - 86400 * 14; // x days, standard API access will probably return only 20 tweets either way
+$query = sprintf('from:dril include:nativeretweets since:%s until:%s', date('Y-m-d', $since), date('Y-m-d', $now));
+
+
+$options = new DrilArchiveOptions;
+// HTTPOptions
+$options->ca_info                 = realpath(__DIR__.'/../config/cacert.pem'); // https://curl.haxx.se/ca/cacert.pem
+$options->user_agent              = 'drilArchive/1.0 +https://github.com/codemasher/dril-archive';
+// DrilArchiveOptions
+$options->builddir                = __DIR__.'/../.build';
+$options->outdir                  = __DIR__.'/../output';
+$options->fromCachedApiResponses  = false;
+$options->fetchFromAPISearch      = true;
+$options->apiToken                = getToken();
+$options->query                   = $query;
+
+
+// we need this one here just for local runs (or repairs...)
+$timelineJSON = __DIR__.'/../.build/dril.json';
+
+// on GitHub actions: clone repo, checkout gh-pages, use previous build
+if(isset($_SERVER['GITHUB_ACTIONS'])){
+#	$timelineJSON = realpath(__DIR__.'/../previous-build/dril.json');
+}
+
+(new DrilArchive($options))->compileDrilTimeline($timelineJSON, true, $since);
+
+
+exit;
+
+/**
+ * attempt to get a bearer token from the environment.
+ */
+function getToken():string{
+
+	// get the token from the environment/config
+	if(isset($_SERVER['GITHUB_ACTIONS'])){
+		return getenv('TWITTER_BEARER');
+	}
+
+	// a dotenv instance for the config
+	$env = (new DotEnv(__DIR__.'/../config', '.env', false))->load();
+
+	return $env->get('TWITTER_BEARER');
+}
